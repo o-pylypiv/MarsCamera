1f1f57da26295fc049b9780cc031a0ed18c60db5
o-pylypiv <bassenia@gmail.com>
2024-08-08 16:35:04 +0300

    Initial Commit

---
+++ MarsCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarsCamera
+//
+//  Created by Olha Pylypiv on 08.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
